3409c60f0573486199142ea41712b69eca65a6c7
mauriciobrito7 <maubot01@gmail.com>
2021-03-27 00:15:16 -0400

    SWR practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import useSWR, { SWRConfig } from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SWRConfig value={{ fetcher }}>
+        <Crimes />
+      </SWRConfig>
     </div>
   );
 }
 
 export default App;
+
+const Crimes = () => {
+  const API =
+    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSWR(API);
+
+  if (error) return <div>{error}</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <>
+      <DisplayCrimes
+        crimes={data}
+        categories={[...new Set(data.map((crime) => crime.category))]}
+      />
+    </>
+  );
+};
+
+const DisplayCrimes = ({ crimes, categories }) => {
+  const [filteredCrimes, setFilteredCrimes] = useState(crimes || "");
+
+  const filterCrimes = (category) => {
+    setFilteredCrimes(crimes.filter((crime) => crime.category === category));
+  };
+
+  return (
+    <>
+      {categories &&
+        categories.map((category) => (
+          <button
+            onClick={() => {
+              filterCrimes(category);
+            }}
+          >
+            {category}
+          </button>
+        ))}
+      <button
+        onClick={() => {
+          setFilteredCrimes(crimes);
+        }}
+      >
+        Reset
+      </button>
+      {filteredCrimes.map((crime) => (
+        <ul key={crime.id}>
+          <li>{crime.id}</li>
+          <li>{crime.category}</li>
+          <li>{crime.location_type}</li>
+          <li>{crime.month}</li>
+        </ul>
+      ))}
+    </>
+  );
+};
